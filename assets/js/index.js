547f191524cae67ebd1445b2f62665cdf7829d9d
john232001 <mrjokermellina@gmail.com>
2024-02-14 17:49:31 +0800

    frontend

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+
+
+new DataTable('#example', {
+    responsive: true,
+    autoWidth: false
+});
+new DataTable('#arb', {
+    responsive: true,
+    autoWidth: false
+});
+new DataTable('#lot', {
+    responsive: true,
+    autoWidth: false
+});
+new DataTable('#asp', {
+    responsive: true,
+    autoWidth: false
+});
+new DataTable('#valuation', {
+    responsive: true,
+    autoWidth: false
+});
+new DataTable('#awardland', {
+    responsive: true,
+    autoWidth: false
+});
+new DataTable('#form', {
+    responsive: true,
+    autoWidth: false
+});
+
+
